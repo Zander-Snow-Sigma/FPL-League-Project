a708fe4f02f9efd20469b2d52d9d815925fb4049
Zander Snow <trainee.zander.snow@sigmalabs.co.uk>
2024-03-28 16:57:47 +0000

    Added docstring

--- test_extract.py
+++ test_extract.py
@@ -4,5 +4,6 @@ from extract import get_league_name
 
 
 def test_get_league_name():
+    """Tests the correct league name is returned."""
     raw_data = {'league': {'name': "test name"}, 'other': 'test'}
     assert get_league_name(raw_data) == "test name"
